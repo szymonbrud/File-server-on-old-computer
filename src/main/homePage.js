723f251a68551c03809db270b657aafe37e2f10f
MateuszM <mateuszmysliwiec03@gmail.com>
2021-02-19 18:18:08 +0100

    I added hiding the options bar

--- src/main/homePage.js
+++ src/main/homePage.js
@@ -6,16 +6,21 @@ const isCheckPass = checkAlreadyAuthentication();
 if (isCheckPass) {
   const burger = document.querySelector('#searchAndBurger');
   const topNav = document.querySelector('#topNav');
+  const conHome = document.querySelector('#containerhome');
+  const back = document.querySelector('#back');
 
   burger.addEventListener('click', function () {
     burger.style.display = 'none';
     topNav.style.transform = 'translateX(0%)';
     topNav.style.transitionDuration = '0.5s';
+    back.style.transform = 'translateX(150%)';
+    back.style.transitionDuration = '0.5s';
   });
-
-  document.addEventListener('touchmove', touchMove);
-
-  function touchMove(ev) {
-    console.log(ev.touches, ev.type);
-  }
+  back.addEventListener('click', () => {
+    burger.style.display = 'block';
+    topNav.style.transform = 'translateX(-100%)';
+    topNav.style.transitionDuration = '0.5s';
+    back.style.transform = 'translateX(-100%)';
+    back.style.transitionDuration = '0.5s';
+  });
 }
